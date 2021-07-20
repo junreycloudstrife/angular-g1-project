e12ca36d010dfc267f6e1c8a35d88d840ad65501
junreycloudstrife <junrey.cloudstrife@gmail.com>
2021-07-21 06:32:52 +0800

    Created new 4 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent }from './second/second.component';
+import { ThirdComponent } from './third/third.component';
+import { FourthComponent } from './fourth/fourth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    ThirdComponent,
+    FourthComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/second/second.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-second',
+    template: `<p>SecondComponent works</p?`,
+    styles:['p {color:red}']
+})
+
+export class SecondComponent{
+    
+}
