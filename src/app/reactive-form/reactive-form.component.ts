c8c78cf127aa80349d020b7c5480b3ae58e4b10d
junreycloudstrife <junrey.cloudstrife@gmail.com>
2021-07-26 07:27:14 +0800

    reactive-form

---
+++ src/app/reactive-form/reactive-form.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-form',
+  templateUrl: './reactive-form.component.html',
+  styleUrls: ['./reactive-form.component.css']
+})
+export class ReactiveFormComponent implements OnInit {
+
+  updateForm = new FormGroup({
+    name: new FormControl(''),
+    brand: new FormControl(''),
+    model: new FormControl('')
+  })
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  updateDevice(){
+    console.log(this.updateForm.value);
+  }
+}
